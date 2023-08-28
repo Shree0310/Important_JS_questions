fe1566e2323e3382f267e9cd2fb1ce65a3db6242
Shree0310 <139523672+Shree0310@users.noreply.github.com>
2023-08-28 18:48:32 +0530

    Update pallindrome.js

--- pallindrome.js
+++ pallindrome.js
@@ -10,5 +10,24 @@ function pallindrome2(str){
   }
   return "Pallindrome";
 }
-
+let str2 = "ollo";
 console.log(pallindrome2(str2));
+
+//using forEach loop
+const pallindrome3 = (str) =>{
+	let isPallindrome = true;
+    str.split("").forEach((c,i)=>{
+      if(c != str[str.length-i-1]){
+	isPallindrome =false;
+      }
+    });
+    
+    return isPallindrome;
+}
+console.log(pallindrome2(str2));
+
+
+//using every
+const pallindrome4 = (str) => str.split("").every((c,i)=>c==str[str.length-i-1]);
+
+console.log(pallindrome4(str2));
