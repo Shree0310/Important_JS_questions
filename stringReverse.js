9bf7f4d8ed72d04ff71ac3dae3c40e341687a51a
Shree0310 <139523672+Shree0310@users.noreply.github.com>
2023-08-28 19:10:07 +0530

    Create stringReverse.js

---
+++ stringReverse.js
@@ -0,0 +1,17 @@
+//Reverse a string
+let str = "apple"
+//using for loop, time: 4 mins
+const reverseString = function(str){
+	let strArr = str.split("");
+  let temp;
+  
+  for(let i=0;i<strArr.length/2;i++){
+  	temp= strArr[i];
+    strArr[i]= strArr[strArr.length-i-1];
+    strArr[strArr.length-i-1]= temp;
+    
+  }
+  return strArr.join("");
+}
+
+console.log(reverseString(str));
