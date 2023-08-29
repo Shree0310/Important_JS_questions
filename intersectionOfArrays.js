c790f4925fe9083ebfda6541106e68e69c4b132b
Shree0310 <139523672+Shree0310@users.noreply.github.com>
2023-08-29 20:54:12 +0530

    Update intersectionOfArrays.js

--- intersectionOfArrays.js
+++ intersectionOfArrays.js
@@ -3,5 +3,27 @@
 //Given two integer arrays nums1 and nums2, return an array of their intersection.
 //Each element in the result must be unique and you may return the result in any order.
 
+var intersection = function(num1,num2){
+	let num1Map ={};
+  
+  for(let i=0;i<num1.length;i++){
+  	if(num1Map[num1[i]]==undefined){
+    	num1Map[num1[i]]=1;
+    }
+  }
+  
+  let arr=[];
+  for(let i=0;i<num2.length;i++){
+  	if(num1Map[num2[i]]==1){
+    	arr.push(num2[i]);
+      num1Map[num2[i]]=0;
+    }
+  }
+  
+  return arr;
+}
+let m1 =[1,2,3];
+let m2 =[3,2,5];
 
+console.log(intersection(m1,m2));
 
