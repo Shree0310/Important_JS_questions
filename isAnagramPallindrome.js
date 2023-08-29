ad279e4a857da3fd8cda0e619880335e657cef05
Shree0310 <139523672+Shree0310@users.noreply.github.com>
2023-08-29 16:59:10 +0530

    Create isAnagramPallindrome.js

---
+++ isAnagramPallindrome.js
@@ -0,0 +1,29 @@
+//write a code and check if any anagram of a string is palindrome or not
+var isAngrmPldrm = function(str){
+	let strMap = {};
+  for(let i=0;i<str.length;i++){
+  	if(strMap[str[i]]){
+    	strMap[str[i]]++;
+    }
+    else{
+    strMap[str[i]] = 1;
+    }
+  }
+  console.log(strMap);
+  // Traverse the count array and if the count array has more than one odd values, return false. Otherwise, return true.
+  let oddCount =0;
+  for(let key in strMap){
+   console.log(key);
+   if(strMap[key]%2 !== 0){
+   	 oddCount++;
+   }
+  }
+  console.log(oddCount);
+  if(oddCount>1){
+  	return false;
+  }
+  else{
+  return true;
+  }
+}
+console.log(isAngrmPldrm("adamm"));
