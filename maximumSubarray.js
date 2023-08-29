77a40c05f110acdc0f114e4e65edaf40a6599a94
Shree0310 <139523672+Shree0310@users.noreply.github.com>
2023-08-29 13:15:02 +0530

    Update maximumSubarray.js

--- maximumSubarray.js
+++ maximumSubarray.js
@@ -12,3 +12,34 @@ var maxSubArray = function(nums){
 }
 let nums2 = [5,4,-1,7,8];
 console.log(maxSubArray(nums2));
+
+//Approach - 2
+//You are given an integer array nums consisting of n elements, and an integer k.
+//Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. 
+
+var findMaxSubArray = function(nums, k){
+	let sum =0;
+  let maxSum = -5000;
+  
+  for(let i=0;i<k;i++){
+  	sum += nums[i];
+  }
+  
+  let j=k;
+  let i=0;
+  maxSum = sum;
+  while(j<nums.length){
+  	sum = sum -nums[i]+nums[j]
+    j++;
+    i++;
+    maxSum = Math.max(sum,maxSum);
+  	
+  }
+  return maxSum;
+}
+
+let nums3 = [1,12,-5,-6,50,3], 
+k = 4
+
+console.log(findMaxSubArray(nums3,k));
+//4 min
