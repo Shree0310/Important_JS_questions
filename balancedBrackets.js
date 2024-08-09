cc7c59e8cfbf321a6023d7745ddaea2abd8edb11
Sowrasree Banerjee <139523672+Shree0310@users.noreply.github.com>
2024-08-09 12:51:07 +0530

    Update balancedBrackets.js

--- balancedBrackets.js
+++ balancedBrackets.js
@@ -1,4 +1,30 @@
 //Balanced Brackets
+//Approach 1
+let areBracketsBalanced = (str) =>{
+	let stack = [];
+  let check;
+  for(let i=0;i<str.length;i++){
+  	if(str[i]== "(" || str[i] == "[" || str[i]=='{'){
+    	stack.push(str[i]);
+      console.log("stack:"+ stack);
+    } else {
+      if(stack.length == 0) return false;
+      console.log("check"+ check);
+	    check = stack.pop();
+      if(check == '[' && str[i] != ']') return false;
+      if(check == '(' && str[i] != ')') return false;
+      if(check == '{' && str[i] != '}') return false;
+		}
+  }
+  	
+    return (stack.length == 0);
+    
+}
+
+console.log(areBracketsBalanced(expr));
+
+
+//Balanced Brackets- Approach 2
 let expr = "([{}])";
 
 const areBracketsBalanced =(str)=>{
