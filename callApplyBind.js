8b48c558a0f21f30cf5f743423c61fa97d066371
Sourashree Banerjee <139523672+Shree0310@users.noreply.github.com>
2026-05-24 19:08:11 +0530

    Refactor examples of call, apply, and bind methods
    Refactor call, apply, and bind examples for clarity.

--- callApplyBind.js
+++ callApplyBind.js
@@ -1,53 +1,52 @@
-let object = {
-	Name: "Soura",
-  City: "Bglr",
-  getIntro: function(){
-  	console.log("Name is " + this.Name + " City is " + this.City);
-  }
-}
-
-let object2 = {
-	Name: "Shree",
-}
-
-object2.__proto__ = object;
-
-/* console.log(object2.Name);
-console.log(object2.City);
-console.log(object2.getIntro());
- */
 let name = {
-	firstName: "Sourashree",
-  lastName: "Banerjee",
-}
-
-//name.printfullName();
-
-let name2 ={
-	firstName: "Sourabh",
-  lastName: "Ganguly"
-}
-
- printfullName = function(hometown, state){
-  //each and every function in javascript has an access to this keyword
-  //the this here refers to the name object
-  	console.log("Full Name is " + this.firstName + " " + this.lastName +" Home town is "+ hometown + " State is " + state);
+  firstName: "Rahul",
+  lastName: "Sharma",
+  printFullName: function() {
+    console.log(this.firstName + " " + this.lastName);
   }
-//call method is used to borrow a function for example here in order to use the printfullName on name2 we will use call, it's first argument tells us where the "this" should point to
-//And the second argument will be the argument that is been passed into the function
-//first argument is reference to this variable
-printfullName.call(name, "Bsp", "CG");
+}
 
-//The only difference btw call and apply methods is how we pass methods
-//Instead of passing the arguments separately as in call method, in  apply method they are passed in an array list
+name.printFullName();
 
-printfullName.apply(name, ["Bhillai", "CG"]);
+let name2 = {
+  firstName: "Rohit",
+  lastName: "Karna",
+}
 
-//Instead of directly calling the function , the bind method binds the method printfullName with an object (name2) and returns us a copy of that function,
-//it returns a function to us that we can use later
+//We can do a function borrowing using call method
+//Very usefull for borrowing functions from other methods
+name.printFullName.call(name2);
 
-let printByBind = printfullName.bind(name, "Bsp", "CG");
-console.log(printByBind);
-//We can invoke the method like a normal method, rather than directly invoking the code  whereever we are writing it:
+//We generally have the methods outside the object
+function printFullName2() {
+    console.log(this.firstName + " " + this.lastName);
+  }
 
-printByBind();
+  printFullName2.call(name2);
+
+  //If we have more parameters in this function
+  let printFullName3 = function(hometown, state) {
+        console.log(this.firstName + " " + this.lastName + "is from" + hometown + state);
+  }
+
+  printFullName3.call(name, "Dehradun", "Uttarakhand");
+
+  //Only difference btw call and apply is apply takes the second argument as a list of arguments
+ 
+ printFullName3.apply(name, ["Dehradun", "Uttarakhand"]);
+
+ //And bind method instead of directly calling this method, the bind method binds the method to the object and creates a copy of the function, which can be invoked later
+
+ let printMyName =   printFullName3.bind(name, "Dehradun", "Uttarakhand");
+
+//returns us a method which can be called later
+//invoke it like a normal method
+printMyName();
+
+
+
+
+
+
+
+
