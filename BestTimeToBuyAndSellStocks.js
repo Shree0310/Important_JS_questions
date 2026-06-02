0590d2597be5f67a142077a8383adc493bd91c15
Sourashree Banerjee <139523672+Shree0310@users.noreply.github.com>
2026-06-02 08:38:49 +0530

    Implement maxProfit function to calculate stock profit

---
+++ BestTimeToBuyAndSellStocks.js
@@ -0,0 +1,13 @@
+var maxProfit = function(prices) {
+    let maxProfit = 0;
+    min = prices[0];
+    for(let i=1; i<prices.length;i++){
+        if(prices[i]<min){
+            min = prices[i];
+        }
+        if(prices[i]-min>maxProfit){
+            maxProfit = prices[i]-min;
+        }
+    }
+    return maxProfit;
+};
