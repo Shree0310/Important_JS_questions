14454358b77d0ffe36527d776f319678e6f476fe
Sourashree Banerjee <139523672+Shree0310@users.noreply.github.com>
2026-06-06 11:57:34 +0530

    Implement recursive sum function
    Added a function to calculate the sum of the first n numbers.

--- arrSum.js
+++ arrSum.js
@@ -5,3 +5,12 @@ function arrSum(n){
 }
 arr = [5, 3, 2, 0, 1, 2];
 console.log(arr.length -1);
+
+
+//sum of first n numbers
+function sum(n) {
+    if (n == 0) return 0;
+    return n + sum(n - 1);
+}
+
+//console.log(sum(10));
