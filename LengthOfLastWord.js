61686bb6d351289ade70ebe28491ee20c404b7d7
Sourashree Banerjee <139523672+Shree0310@users.noreply.github.com>
2026-06-24 18:34:48 +0530

    Update LengthOfLastWord.js

--- LengthOfLastWord.js
+++ LengthOfLastWord.js
@@ -19,3 +19,19 @@ var lengthOfLastWord = function(s) {
 
 let s = "   fly me   to   the moon  ";
 console.log(lengthOfLastWord(s));
+
+//approach 2
+var lengthOfLastWord = function(s) {
+    let n = s.length - 1;
+    let count = 0;
+    while(n>=0){
+        if(s[n] != " "){
+            count++;
+        }
+        else if(s[n] === " " && count >0){
+            break;
+        }
+        n--;
+    }
+    return count;
+};
