ea2b5ed438ef5ece55c56ed90b1c803a4847c590
Sourashree Banerjee <139523672+Shree0310@users.noreply.github.com>
2026-06-26 11:51:13 +0530

    Optimize vowel and consonant frequency calculation
    Optimized the frequency calculation of vowels and consonants by using Object.keys to reduce the number of iterations.

--- MostFreqVowelConsonant.js
+++ MostFreqVowelConsonant.js
@@ -39,3 +39,38 @@ var maxFreqSum = function(s) {
 // The vowels are: 'u' (frequency 1), 'e' (frequency 2). The maximum frequency is 2.
 // The consonants are: 's' (frequency 4), 'c' (frequency 2). The maximum frequency is 4.
 // The output is 2 + 4 = 6.
+
+
+//Optimisation
+//using Onject.Keys the second loop rather than looping through the entire word again
+var maxFreqSum = function(s) {
+    let charMap = {};
+    //Fill the map with the chars count
+    for(let i =0; i<s.length; i++){
+        if(charMap[s[i]]){
+            charMap[s[i]]++;
+        }
+        else{
+            charMap[s[i]] = 1;
+        }
+    }
+
+    //getting the max count of vowels and consonants
+    let maxVowels = 0;
+    let maxConsonants = 0;
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    let mapKeys = Object.keys(charMap);
+    for(let i=0; i<mapKeys.length; i++){
+        if(vowels.includes(mapKeys[i])){
+            if(charMap[mapKeys[i]]> maxVowels){
+                maxVowels = charMap[mapKeys[i]];
+            }
+        }
+        else{
+            if(charMap[mapKeys[i]] > maxConsonants){
+                maxConsonants = charMap[mapKeys[i]];
+            }
+        }
+    }
+    return maxVowels + maxConsonants;
+};
